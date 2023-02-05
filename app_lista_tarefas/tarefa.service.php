236fe2525fa2970ad3657132d945ba887462a56f
LuizEddie <contato.luiz1997@gmail.com>
2023-02-05 11:48:13 -0300

    Finalizado Lista de Tarefas

---
+++ app_lista_tarefas/tarefa.service.php
@@ -0,0 +1,64 @@
+<?php
+    class TarefaService{
+
+        private $conexao;
+        private $tarefa;
+
+        public function __construct(Conexao $conexao, Tarefa $tarefa)
+        {
+            $this->conexao = $conexao->conectar();
+            $this->tarefa = $tarefa;    
+        }
+
+        public function inserir(){
+            $query = 'INSERT INTO tb_tarefas (tarefa) VALUES (:tarefa)';
+            $stmt = $this->conexao->prepare($query);
+            $stmt->bindValue(":tarefa", $this->tarefa->__get("tarefa"));
+            $stmt->execute();
+        }
+
+        public function recuperar(){
+            $query = "SELECT tt.id, tt.tarefa, ts.status FROM tb_tarefas as tt
+            LEFT JOIN tb_status as ts ON tt.id_status = ts.id";
+            $stmt = $this->conexao->prepare($query);
+            $stmt->execute();
+            
+            return $stmt->fetchAll(PDO::FETCH_OBJ);
+        }
+
+        public function recuperarPendente(){
+            $query = "SELECT tt.id, tt.tarefa, ts.status FROM tb_tarefas as tt
+            LEFT JOIN tb_status as ts ON tt.id_status = ts.id
+            WHERE tt.id_status = 1";
+            $stmt = $this->conexao->prepare($query);
+            $stmt->execute();
+            
+            return $stmt->fetchAll(PDO::FETCH_OBJ);
+        }
+
+        public function atualizar(){
+            $query = "UPDATE tb_tarefas SET tarefa = :tarefa WHERE id = :id";
+            $stmt = $this->conexao->prepare($query);
+            $stmt->bindValue(":id", $this->tarefa->__get("id"));
+            $stmt->bindValue(":tarefa", $this->tarefa->__get("tarefa"));
+            return $stmt->execute();
+        }
+
+        public function remover(){
+            $query = "DELETE FROM tb_tarefas WHERE id = :id";
+            $stmt = $this->conexao->prepare($query);
+            $stmt->bindValue(":id", $this->tarefa->__get("id"));
+            $stmt->execute();
+        }
+
+        public function marcarRealizada(){
+            $query = "UPDATE tb_tarefas SET id_status = :id_status WHERE id = :id";
+            $stmt = $this->conexao->prepare($query);
+            $stmt->bindValue(":id", $this->tarefa->__get("id"));
+            $stmt->bindValue(":id_status", $this->tarefa->__get("id_status"));
+            $stmt->execute();
+        }
+
+    }
+
+?>
---
+++ app_lista_tarefas/tarefa_controller.php
@@ -0,0 +1,87 @@
+<?php
+    require("../../app_lista_tarefas/tarefa.model.php");
+    require("../../app_lista_tarefas/tarefa.service.php");
+    require("../../app_lista_tarefas/conexao.php");
+
+    $acao = isset($_GET['acao']) ? $_GET['acao'] : $acao;
+
+    if($acao == 'inserir'){
+        $tarefa = new Tarefa();    
+        $tarefa->__set('tarefa', $_POST['tarefa']);
+    
+        $conexao = new Conexao();
+    
+        $tarefaService = new TarefaService($conexao, $tarefa);
+        $tarefaService->inserir();
+    
+        header('Location: nova_tarefa.php?inclusao=1');
+    }
+
+    if($acao == 'recuperar'){
+        $tarefa = new Tarefa();
+        $conexao = new Conexao();
+        $tarefaService = new TarefaService($conexao, $tarefa);
+
+        $tarefas = $tarefaService->recuperar();
+    }
+
+    if($acao == 'atualizar'){
+        $tarefa = new Tarefa();
+        $conexao = new Conexao();
+
+        $tarefa->__set("id", $_POST['id']);
+        $tarefa->__set("tarefa", $_POST['tarefa']);
+        $tarefaService = new TarefaService($conexao, $tarefa);
+
+        if($tarefaService->atualizar()){
+
+            if(isset($_GET['pag']) && $_GET['pag'] == 'index'){
+                header('location: index.php');
+            }else{
+                header('location: todas_tarefas.php');
+            }
+        }
+    }
+
+    if($acao == 'remover'){
+        $tarefa = new Tarefa();
+        $conexao = new Conexao();
+
+        $tarefa->__set("id", $_GET['id']);
+        $tarefaService = new TarefaService($conexao, $tarefa);
+
+        $tarefaService->remover();
+        if(isset($_GET['pag']) && $_GET['pag'] == 'index'){
+            header('location: index.php');
+        }else{
+            header('location: todas_tarefas.php');
+        }
+        
+    }
+
+    if($acao == 'marcarRealizada'){
+        $tarefa = new Tarefa();
+        $conexao = new Conexao();
+
+        $tarefa->__set("id", $_GET['id']);
+        $tarefa->__set("id_status", 2);
+        $tarefaService = new TarefaService($conexao, $tarefa);
+
+        $tarefaService->marcarRealizada();
+        if(isset($_GET['pag']) && $_GET['pag'] == 'index'){
+            header('location: index.php');
+        }else{
+            header('location: todas_tarefas.php');
+        }
+    }
+
+    if($acao == 'recuperarPendentes'){
+        $tarefa = new Tarefa();
+        $conexao = new Conexao();
+        $tarefaService = new TarefaService($conexao, $tarefa);
+
+        $tarefas = $tarefaService->recuperarPendente();
+    }
+
+   
+?>
